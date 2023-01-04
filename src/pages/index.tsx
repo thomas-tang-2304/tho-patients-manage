1432820a73923348bf7fa1e8c1c3e6f108d9cfb4
Hong Tang | Tester <77006112+hongtang-digiex@users.noreply.github.com>
2023-01-04 14:49:46 +0700

    configured tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import Head from 'next/head';
 import Image from 'next/image';
 
@@ -6,7 +7,6 @@ import styles from '@/styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-
       <Head>
         <title>TypeScript starter for Next.js</title>
         <meta
