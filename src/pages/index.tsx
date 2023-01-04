ef72adb3da7661e8b67dd76bc9b417aa78af015e
Hong Tang | Tester <77006112+hongtang-digiex@users.noreply.github.com>
2023-01-04 14:06:17 +0700

    configured tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import styles from '@/styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
+
       <Head>
         <title>TypeScript starter for Next.js</title>
         <meta
@@ -15,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main className={`${styles.main}`}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
