d86ee9f8a00da105f406bbdb0e7798bfea84e250
Minh Thong | Intern <121416479+minhthong-digiex@users.noreply.github.com>
2023-01-09 16:24:04 +0700

    login

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,8 +1,6 @@
 /* eslint-disable prettier/prettier */
-import React from 'react'
+import React from 'react';
 
 export default function products() {
-    return (
-        <div>products</div>
-    )
+  return <div>products</div>;
 }
