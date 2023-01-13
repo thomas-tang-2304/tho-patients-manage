0f89a26e8e2dab98b56c2a3143ca72473d165eff
Hong Tang | Tester <77006112+hongtang-digiex@users.noreply.github.com>
2023-01-13 09:25:04 +0700

    add modal and re-edit UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
@@ -8,10 +9,11 @@ module.exports = {
     theme: {
       colors: {
         'primary-background-color': '#064055',
-        'primary-color': '#ffb451'
+        'primary-color': '#ffb451',
       },
     },
   },
+  plugins: [require('@tailwindcss/line-clamp')],
 
   darkMode: `class`,
 };
